e2436e29759777bdf479b372e280ffee8e609f46
sd3ntato <valeriomariani52@gmail.com>
2021-11-17 12:04:30 +0100

    moved tasks definitions

--- par_utils/tasks.h
+++ par_utils/tasks.h
@@ -1,3 +1,9 @@
+#ifndef linear_algebra
+#define linear_algebra
+#include "linear_algebra.h"
+#endif
+#include "math.h"
+
 class Task
 {
 public:
@@ -14,6 +20,42 @@ private:
   float *res_ptr;
 
 public:
-  Dot_task(int s, int st, float *x, float *y, float *r);
-  void execute();
+  Dot_task(int s, int st, float *x, float *y, float *r)
+  {
+    start = s;
+    stop = st;
+    v1 = x;
+    v2 = y;
+    res_ptr = r;
+  }
+  void execute()
+  {
+    *res_ptr = dot(start, stop, v1, v2);
+  }
+};
+
+class Comp_state_task : public Task
+{
+private:
+  int start, stop, Nu;
+  float *x_rec, *x_in, *x, **Win;
+
+public:
+  Comp_state_task(int start, int stop, int Nu, float *x_rec, float *x_in, float **Win, float *x)
+  {
+    this->start = start;
+    this->stop = stop;
+    this->x_rec = x_rec;
+    this->x_in = x_in;
+    this->Nu = Nu;
+    this->Win = Win;
+    this->x = x;
+  }
+  void execute()
+  {
+    for (int i = start; i < stop; i++)
+    {
+      x[i] = tanh(x_rec[i] + x_in[i] + Win[i][Nu]); // Win[i]-> b[i] TODO
+    }
+  }
 };
