8ed73c78aef448a95962986b4a899c3bbe42c63c
sd3ntato <valeriomariani52@gmail.com>
2021-11-17 17:19:51 +0100

    prot_queue pops values now

--- par_utils/prot_queue.h
+++ par_utils/prot_queue.h
@@ -39,7 +39,7 @@ inline T queue<T>::pop()
   std::unique_lock<std::mutex> lock(this->d_mutex);
   this->d_condition.wait(lock, [=]
                          { return !this->d_queue.empty(); });
-  T rc(std::move(this->d_queue.back()));
+  T rc = this->d_queue.back();
   this->d_queue.pop_back();
   d_condition.notify_all();
   return rc;
