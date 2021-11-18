df08b1bda5223957d6edb7bda8d460f449e94197
sd3ntato <valeriomariani52@gmail.com>
2021-11-18 12:41:36 +0100

    adpted dot_task to fit map2 function

--- par_utils/tasks.h
+++ par_utils/tasks.h
@@ -15,22 +15,25 @@ class Dot_task : public Task
 private:
   int start;
   int stop;
-  float *v1;
-  float *v2;
+  int i;
+  float **M;
+  float *y;
   float *res_ptr;
 
 public:
-  Dot_task(int s, int st, float *x, float *y, float *r)
+  Dot_task() = default;
+  Dot_task(int start, int stop, int i, float **M, float *y, float *r)
   {
-    start = s;
-    stop = st;
-    v1 = x;
-    v2 = y;
-    res_ptr = r;
+    this->start = start;
+    this->stop = stop;
+    this->i = i;
+    this->M = M;
+    this->y = y;
+    this->res_ptr = r;
   }
   void execute()
   {
-    *res_ptr = dot(start, stop, v1, v2);
+    res_ptr[i] = dot(start, stop, M[i], y);
   }
 };
 
@@ -82,6 +85,62 @@ public:
     for (int i = start; i < stop; i++)
     {
       r[i] = (v[i] / k);
+    }
+  }
+};
+
+class Compute_new_Wout : public Task
+{
+private:
+  int start, stop, i;
+  float **Wout, **Wold, *d, *y, *k;
+
+public:
+  Compute_new_Wout() = default;
+  Compute_new_Wout(int start, int stop, int i, float **Wout, float **Wold, float *d, float *y, float *k)
+  {
+    this->start = start;
+    this->stop = stop;
+    this->i = i;
+    this->Wout = Wout;
+    this->Wold = Wold;
+    this->d = d;
+    this->y = y;
+    this->k = k;
+  }
+  void execute()
+  {
+    for (int j = start; j < stop; j++)
+    {
+      Wout[i][j] = Wold[i][j] + (d[i] - y[i]) * k[j];
+    }
+  }
+};
+
+class Compute_new_P : public Task
+{
+private:
+  int start, stop, i;
+  float **P, **Pold, *k, *z, l;
+
+public:
+  Compute_new_P() = default;
+  Compute_new_P(int start, int stop, int i, float **P, float **Pold, float *k, float *z, float l)
+  {
+    this->start = start;
+    this->stop = stop;
+    this->i = i;
+    this->P = P;
+    this->Pold = Pold;
+    this->k = k;
+    this->z = z;
+    this->l = l;
+  }
+  void execute()
+  {
+    for (int j = start; j < stop; j++)
+    {
+      P[i][j] = (Pold[i][j] - k[i] * z[j]) * 1 / l;
     }
   }
 };
